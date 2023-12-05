36f9ab63c7ccc706d557fb7e3fcb5b190ab51ed3
ocb3916 <dhchanbin1004@gmail.com>
2023-12-05 17:40:03 +0900

    게시판 제작중

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const app = express();
 // 포트번호 설정
 const port = 5000;
 
+app.use(express.static('기말제출용'));
+
 // http 서버 실행
-app.listen(port,() =>{
+app.listen(port, () => {
     console.log("서버가 정상적으로 실행되었습니다.");
 });
 
 // http:/localhost:5000/ 경로로 접근하면
-app.get("/",(request, response)=>{
-    response.send("수정되었습니다.");
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/기말제출용/index.html");
 });
